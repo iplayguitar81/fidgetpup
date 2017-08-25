e66d013b0b190fdd7b9fe52957c85e13e2df95e4
iplayguitar81 <iplayguitar81@yahoo.com>
2017-08-25 10:45:19 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <br/>
 <div class="panel-footer">
-    Trailblazersfans.com Copyright {{date("Y")}}
+    FidgetSpinnerDog.com &amp; GhostHits Copyright {{date("Y")}}
     <br/>
     <a class="text-center" href="{{ route('disclaimer') }}">Site Disclaimer</a>
 </div>
