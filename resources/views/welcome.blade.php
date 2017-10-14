d4fa694bab4e50d3b17e86f72fee5330bcb95dcf
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-14 00:19:38 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,7 +114,7 @@
       {{--<p>  {{strip_tags((str_limit($item->body, 100)))}}...</p>--}}
             <a class="btn btn-success btn-md active" href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">Continue Reading</a>
 
-        <button type="submit" class="btn center-block btn-md" onclick="window.location='{{ route('posts.show', [$item->id, str_slug($item->title)]) }}/#fb-comments-show';" ><i class="fa fa-btn fa-comment"></i> Comments</button>
+        <button type="submit" class="btn center-block btn-md" onclick="window.location='{{ route('posts.show', [$item->id, str_slug($item->title)]) }}/#fb-comments-show';" ><span class="count-icon"><i class="fa fa-btn fa-comment"></i><span class="count">0</span></span>Comments</button>
         <br/>
         <br/>
 
