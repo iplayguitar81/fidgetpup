86fe5d84ff50beacf9a13a1ca5ee1e278097810e
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-14 00:34:04 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1450,11 +1450,12 @@ em.caption-em{
     ga('send', 'pageview');
 
 </script>
+<div id="fb-root"></div>
 <script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=590306801102510";
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1863073990661184";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
