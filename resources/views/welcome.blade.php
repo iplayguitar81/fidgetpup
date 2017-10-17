b970ac3f1699f2c63ec54cf49849d162046b92ad
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-17 00:42:22 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,7 +114,7 @@
       {{--<p>  {{strip_tags((str_limit($item->body, 100)))}}...</p>--}}
             <a class="btn btn-success btn-lg active" href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">Continue Reading</a>
 
-        <button type="submit" class="btn center-block btn-md" onclick="window.location='{{ route('posts.show', [$item->id, str_slug($item->title)]) }}/#fb-comments-show';" ><span class="count-icon"><i class="fa fa-2x fa-comment"></i> <span class="fb-comments-count" data-href="https://dev.fidgetspinnerdog.com/posts/{{$item->id}}/{{str_slug($item->title)}}"></span></span> Comments </button>
+        <span class="count-icon" onclick="window.location='{{ route('posts.show', [$item->id, str_slug($item->title)]) }}/#fb-comments-show';"><i class="fa fa-2x fa-comment"></i> <span class="fb-comments-count" data-href="https://dev.fidgetspinnerdog.com/posts/{{$item->id}}/{{str_slug($item->title)}}"></span> Comments </span>
 
         <br/>
         <br/>
