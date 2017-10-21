f0112f82d8ec34cdc74c8f89096669a65d324eb9
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-20 22:38:09 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -3,8 +3,6 @@
 @section('content')
 
 
-
-
     <h1 class="article-title-show">news by category</h1>
     <br/>
 
@@ -59,7 +57,7 @@
 
 
                                 <h2 class="secondary-posts-title"><a class="" href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">{{ ucwords($item->title)}}</a></h2>
-                                <p class="text-muted">Written by <a href="#">{{$author}}</a> on {{gameDate($item->created_at)}} </p>
+                                <p class="text-muted">Written by <a href="#">{{written_by($item->user_id)}}</a> on {{gameDate($item->created_at)}} </p>
                                 <p class="text-muted">{{$item->subHead}}</p>
 
                                 <p>{{$variable}}...<a class="" href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">READ MORE</a></p>
