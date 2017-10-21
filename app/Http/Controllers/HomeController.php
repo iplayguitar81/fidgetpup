d800b80fd82dd5e1d84089fd9ec6bb95e59bba50
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-20 22:08:02 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,5 +35,16 @@ class HomeController extends Controller
     }
 
 
+
+    function gameDate($date) {
+
+        $gamedate = new DateTime($date, new DateTimeZone('America/Los_Angeles'));
+
+        $game_date = date_sub($date, date_interval_create_from_date_string('3 hour'));
+        $game_date = $game_date->format('M jS Y');
+
+        return $gamedate;
+
+    }
  
 }
