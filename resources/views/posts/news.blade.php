6e2736029454bf043b11e2a4e3e191bf53344925
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-20 22:20:28 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/news.blade.php
+++ resources/views/posts/news.blade.php
@@ -36,20 +36,10 @@
                         @endif
 
 
-                        @php
-                            $game_date = new DateTime($item->date, new DateTimeZone('America/Los_Angeles'));
-                            $game_date = date_sub($game_date, date_interval_create_from_date_string('3 hour'));
-                            $game_date = $game_date->format('l F dS Y g:i a');
-                        @endphp
-
-
 
                         @php
                             $variable= strip_tags($item->body);
                             $variable =substr($variable,0, 50);
-                        $game_date = new DateTime($item->created_at, new DateTimeZone('America/Los_Angeles'));
-                            $game_date = date_sub($game_date, date_interval_create_from_date_string('3 hour'));
-                            $game_date = $game_date->format('M jS Y');
                         @endphp
 
 
