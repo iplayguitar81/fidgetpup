f2db2625bdb494201cd614354a55f41de01b7d4e
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-20 22:11:09 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -4,10 +4,10 @@ function gameDate($date) {
 
 $gamedate = new DateTime($date, new DateTimeZone('America/Los_Angeles'));
 
-$game_date = date_sub($date, date_interval_create_from_date_string('3 hour'));
+$game_date = date_sub($gamedate, date_interval_create_from_date_string('3 hour'));
 $game_date = $game_date->format('M jS Y');
 
-return $gamedate;
+return $game_date;
 
 }
 
