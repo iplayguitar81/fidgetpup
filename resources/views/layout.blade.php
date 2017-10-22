767895089651581f26b4f959d2aa47bed115a555
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-21 22:15:39 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -63,8 +63,8 @@ ul.pagination{display:inline-block;padding:0;margin:0;font-size:1.2em;color:#449
 }
 .article-title-show {
     color: silver;
-    text-shadow: #000000 2px 1px 2px;
-    font-size: 2.8em !important;
+    text-shadow: #000000 1px 1px;
+    font-size: 1.8em !important;
     text-align: center;
     font-family: eightybitty;
 }
