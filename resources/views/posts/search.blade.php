3098a7f47362bf8b98015cdbf3220ec29017c863
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-23 22:06:59 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -18,15 +18,6 @@
 @else
     @foreach($results2 as $result)
 
-        <?
-
-        $variable= strip_tags($result->body);
-        $variable =substr($variable,0, 50);
-        // $variable = (str_limit($item->body, 100));
-        // $variable= htmlentities($variable);
-        ?>
-
-
         <article>
         <h3><a href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">{{$result->title}}</a></h3>
         <p>Written&nbsp;
@@ -40,7 +31,7 @@
             <span class="text-lowercase">{{$result->created_at->format('M dS, Y')}}</span>
         </p>
 
-        <p>{{$variable}}...     <a class="btn btn-success btn-md active" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">Read More</a></p>
+        <p>{{snippet($result->body)}}...     <a class="btn btn-success btn-md active" href="{{ route('posts.show', [$result->id, str_slug($result->title)]) }}">Read More</a></p>
 
 
     </article>
