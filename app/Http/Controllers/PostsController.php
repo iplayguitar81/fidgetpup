39cb735e498993c0a04a94ee1fc4455b0b094b8a
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-24 21:07:59 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -638,7 +638,7 @@ class PostsController extends Controller
 
         $results2 = $query->all();
 
-     //   $results_empty = $results2->isEmpty();
+      $results_empty = $results2->isEmpty();
 
 
         return view('posts.search', compact('results2','search', 'results_empty'));
