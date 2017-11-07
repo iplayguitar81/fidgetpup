53e21ade2bc9a672698209b36f6ee502fbcef332
iplayguitar81 <iplayguitar81@yahoo.com>
2017-11-06 18:09:48 -0800

    get win 10 laravel started yeahhhhhh

--- resources/lang/en/posts.php
+++ resources/lang/en/posts.php
@@ -6,8 +6,8 @@ return [
 'body' => 'Body',
 'imgPath' => 'Image Path',
 'category' => 'Category',
-'main_article' => 'Main Article?',
-'published' => 'Published?',
+'main_article' => 'Main Article',
+'published' => 'Published',
 
 'mainImg_caption' => 'Main Image Caption',
 
